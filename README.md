541dd1dcba2b916e8f42125b3f7a53992f4d162b
rudygzl <bloofbbl@gmail.com>
2020-01-14 16:36:42 +0100

    new carousel, readme edit

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # javascript-course
 
-This is most of my WIP javascript tests/projects. CSS Framework = TailwindCSS
+## This is most of my WIP javascript tests/projects. CSS Framework = TailwindCSS
 
 Tutorial to import TailwindCSS to my project =
 
 - Clone the git file
 - Open VSCODE with my file
 - Open a vscode terminal
-- Type "npm init -y" to initialize the package.json that will help execute the script to build TAILWINDCSS everytime you use advanced customization
-- In "scripts" section in the package.json file, type = "build:css": "tailwind build src/style.css -o dist/style.css" (including double quotes)
-- Type "npm install tailwindcss" if you want the build script to work (that will install all tailwindcss node modules)
-- Type "npx tailwind init" to generate tailwind.config.js file
-- (This will help center the container) Replace tailwind.config.js file content by 
+- Type "**npm init -y**" to initialize the package.json that will help execute the script to build TAILWINDCSS everytime you use advanced customization
+- In "**scripts**" section in the package.json file, type = **"build:css": "tailwind build src/style.css -o dist/style.css"** (including double quotes)
+- Type "**npm install tailwindcss**" if you want the build script to work (that will install all tailwindcss node modules)
+- Type "**npx tailwind init**" to generate tailwind.config.js file
+- (This will help center the container) Replace **tailwind.config.js** file content by 
 
+```
 module.exports = {
   theme: {
     container: {
@@ -23,5 +24,6 @@ module.exports = {
   variants: {},
   plugins: []
 }
+```
 
 That should be it! If you want to run the build script after some customizations in the tailwind.config.js or @apply customs in src/style.css stylesheet, then type "npm run build:css"
--- src/carousel.js
+++ src/carousel.js
@@ -1,15 +1,67 @@
 /* carousel */
 let carousel = document.getElementById("carousel");
 let suivant = document.getElementById("suivant");
+let precedent = document.getElementById("precedent");
+let infoCarousel = document.getElementById("infoCarousel");
 // Vous aurez besoin de ce genre de choses :// $carousel.style.borderColor = "#f00";
 function next() {
     let i = 0
     let ok = false;
-    while (i <= 3 && !ok) {
+    while (i <= carousel.children.length - 1 && !ok) {
         if (!carousel.children[i].classList.contains("hidden")) {
+            for (let j = 0; j <= carousel.children.length - 1; j++) {
+                if (j === i + 1 || j === 0 && i === carousel.children.length - 1) {
+                    carousel.children[j].classList.remove("hidden");
+                } else {
+                    carousel.children[j].classList.add("hidden");
+                }
+            }
             ok = true;
-            
         }
+        i++
+    }
+}
+
+function prev() {
+    let i = 0
+    let ok = false;
+    while (i <= carousel.children.length - 1 && !ok) {
+        if (!carousel.children[i].classList.contains("hidden")) {
+            for (let j = 0; j <= carousel.children.length - 1; j++) {
+                if (j === i - 1 || j === carousel.children.length - 1 && i === 0) {
+                    carousel.children[j].classList.remove("hidden");
+                } else {
+                    carousel.children[j].classList.add("hidden");
+                }
+            }
+            ok = true;
+        }
+        i++
+    }
+}
+
+function addCase() {
+    carousel.innerHTML += `<li class="inline-block border-box p-2 w-full h-full font-semibold flex justify-center">Case ${carousel.children.length + 1}</li>`;
+    infoCarousel.innerHTML = `<span class="text-white font-bold">La case ${carousel.children.length} a bien été ajoutée !</span>`
+    setTimeout(function () {
+        infoCarousel.innerHTML = ""
+    }, 2000);
+
+}
+
+function removeCase() {
+    if (carousel.children.length !== 1) {
+        let oldRemove = carousel.children.length;
+        carousel.lastElementChild.remove();
+        carousel.lastElementChild.classList.remove("hidden");
+        infoCarousel.innerHTML = `<span class="text-white font-bold">La case ${oldRemove} a bien été supprimée !</span>`
+        setTimeout(function () {
+            infoCarousel.innerHTML = ""
+        }, 2000);
+    } else {
+        infoCarousel.innerHTML = `<span class="text-white font-bold">Il doit rester au moins une case</span>`
+        carousel.innerHTML = `<li class="inline-block border-box p-2 w-full h-full font-semibold flex justify-center">Case 1</li>`;
     }
 }
 suivant.onclick = next;
+precedent.onclick = prev;
